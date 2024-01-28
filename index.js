4c10de8320866a071c643a253f735550d40dc844
Desmond51 <desmondtekum9@gmail.com>
2024-01-28 12:02:30 +0100

    compared the properties of two objects

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-const address1= showAddress("Clarks quarter","Buea", "SW0948")
-console.log(address)
-function showAddress(street,city, zipCode ){
-  return {
-    street,
-    city,
-    zipCode
-  }
+// const address1= showAddress("Clarks quarter","Buea", "SW0948")
+// console.log(address)
+// function showAddress(street,city, zipCode ){
+//   return {
+//     street,
+//     city,
+//     zipCode
+//   }
   
+// }
+// showAddress(address);
+
+// let address = new ShowAddress("Malingo", "Buea", "SW05484");
+// console.log(address);
+// function ShowAddress(street, city, zipCode){
+//   this.street = street,
+//   this.city = city,
+//   this.zipCode = zipCode
+
+// }
+
+// Address()
+
+
+let address2 = new Address('a','b', 'c','d');
+let address3 = new Address("a", "b", "c", "d");
+
+let equalAddress = new areEqual();
+console.log(equalAddress);
+
+function areEqual(address2, address3){
+  return Object.keys( address2 ).every( function( prop ) {
+    return address3.hasOwnProperty( prop );
+  });
+
 }
-showAddress(address);
 
-let address = new ShowAddress("Malingo", "Buea", "SW05484");
-console.log(address);
-function ShowAddress(street, city, zipCode){
-  this.street = street,
-  this.city = city,
-  this.zipCode = zipCode
-
+function Address(street, city, zipCode){
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
 }
-
-Address()
